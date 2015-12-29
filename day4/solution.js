b9d4253be8979da0f73bd52ab519ec157066fc8f
John Beech <github@mkv25.net>
2015-12-29 16:06:18 +0000

    Added calculations for 6 length keys

--- day4/solution.js
+++ day4/solution.js
@@ -3,31 +3,56 @@ var md5 = require('md5');
 
 console.log('Day 4');
 
-if (test('abcdef', 609043) && test('pqrstuv', 1048970)) {
+if (test('abcdef', 609043, generatePattern(5, '0')) && test('pqrstuv', 1048970, generatePattern(5, '0'))) {
     read(__dirname + '/input.txt', 'utf8')
-        .then(solve)
+        .then(solveFor(5))
         .then(report)
         .catch(error);
+
+    read(__dirname + '/input.txt', 'utf8')
+        .then(solveFor(6))
+        .then(report)
+        .catch(error);
+} else {
+    console.log('Start-up tests failed');
 }
 
-function solve(input) {
+function solveFor(length) {
+    return function(secret) {
+        var pattern = generatePattern(length, '0');
+        return solve(secret, pattern);
+    };
+}
+
+function generatePattern(length, char) {
+    var pattern = '';
+    while (length > 0) {
+        pattern += char;
+        length--;
+    }
+    return pattern;
+}
+
+function solve(input, pattern) {
     var secret = input;
     var number = 0;
-    while (test(secret, number) == false) {
+    while (test(secret, number, pattern) == false) {
         number++;
-        if (number % 1000 === 0) {
+        if (number % 10000 === 0) {
             console.log('Tested', number, 'entries');
         }
     };
 
     return {
+        'Secret': secret,
+        'Pattern': pattern,
         'Solved number': number
     };
 }
 
-function test(secret, number) {
+function test(secret, number, pattern) {
     var hash = md5(secret + number);
-    return (hash.indexOf('00000') === 0);
+    return (hash.indexOf(pattern) === 0);
 }
 
 function report(summary) {
