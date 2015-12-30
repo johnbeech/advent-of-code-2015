5e654980d3d888b1148f34d42d82ddac5f30e3cf
John Beech <github@mkv25.net>
2015-12-30 11:51:17 +0000

    Created solution for Day 8.

--- day8/solution.js
+++ day8/solution.js
@@ -1,12 +1,42 @@
 var read = require('../lib/read');
 
+console.log('Day 8');
 read(__dirname + '/input.txt', 'utf8')
+    .then(split)
     .then(solve)
     .then(report)
     .catch(error);
 
+function split(input) {
+    var lines = input.replace(/\r/g, '').split('\n');
+    return Promise.accept(lines);
+}
+
 function solve(lines) {
-    throw 'Not implemented';
+    var inputCount = 0;
+    var memoryCount = 0;
+    var parsed = lines.map(function(line) {
+        inputCount = inputCount + line.length;
+        try {
+            var parsedLine = line;
+            parsedLine = parsedLine.slice(1, -1);
+            parsedLine = parsedLine.replace(/\\\\/g, '~');
+            parsedLine = parsedLine.replace(/\\\"/g, '-');
+            parsedLine = parsedLine.replace(/\\x[A-Fa-f0-9]{2}/g, '#');
+            memoryCount = memoryCount + parsedLine.length;
+            console.log([line.length, line].join(' '));
+            console.log([parsedLine.length, '', parsedLine].join(' '));
+            return parsedLine;
+        } catch (ex) {
+            console.log('Unable to parse', line, ex);
+            return line;
+        }
+    });
+    return {
+        'String length from input': inputCount,
+        'String length in memory': memoryCount,
+        'Difference': inputCount - memoryCount
+    }
 }
 
 function report(summary) {
