a22885e4b1ab0e7dcc974a3681a4aeb62b6259ad
John Beech <github@mkv25.net>
2015-12-30 21:52:36 +0000

    Created solution for Day 10

--- day10/solution.js
+++ day10/solution.js
@@ -5,8 +5,43 @@ read(__dirname + '/input.txt', 'utf8')
     .then(report)
     .catch(error);
 
-function solve(lines) {
-    throw 'Not implemented';
+function solve(input) {
+    for (var i = 0; i < 40; i++) {
+        input = lookAndSay(input);
+    }
+    return {
+        result: input,
+        length: input.length
+    };
+}
+
+function lookAndSay(input) {
+    var look = [input[0]];
+    var say = [1];
+    var char;
+    for (var i = 1; i < input.length; i++) {
+        char = input[i];
+        if (last(look) === char) {
+            last(say, last(say) + 1);
+        } else {
+            look.push(char);
+            say.push(1);
+        }
+    }
+
+    var digits = '';
+    for (var j = 0; j < look.length; j++) {
+        digits = digits + say[j] + look[j];
+    }
+
+    return digits;
+}
+
+function last(array, value) {
+    if (value) {
+        array[array.length - 1] = value;
+    }
+    return array[array.length - 1];
 }
 
 function report(summary) {
