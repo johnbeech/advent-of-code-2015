50856080801b8e2dbee494c83e7be596c23a3426
John Beech <github@mkv25.net>
2015-12-30 13:00:53 +0000

    Added parsing to solution

--- day9/solution.js
+++ day9/solution.js
@@ -1,12 +1,32 @@
 var read = require('../lib/read');
 
+var locations = {};
+
 read(__dirname + '/input.txt', 'utf8')
+    .then(split)
     .then(solve)
     .then(report)
     .catch(error);
 
+function split(input) {
+    var lines = input.replace(/\r/g, '').split('\n');
+    return Promise.accept(lines);
+}
+
+function parse(lines) {
+    var matcher = /([A-z]+) to ([A-z]+) = (\d+)/;
+    return lines.map(function(line) {
+        var matches = line.match(matcher);
+        return {
+            start: matches[1],
+            end: matches[2],
+            distance: matches[3]
+        };
+    });
+}
+
 function solve(lines) {
-    throw 'Not implemented';
+    return lines;
 }
 
 function report(summary) {
