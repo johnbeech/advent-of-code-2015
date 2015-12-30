f17438bb276c4e1c7e358c1f91ff3005eea224bc
John Beech <github@mkv25.net>
2015-12-30 20:43:05 +0000

    Reproduced results from example

--- day9/solution.js
+++ day9/solution.js
@@ -1,4 +1,5 @@
 var read = require('../lib/read');
+var write = require('../lib/write');
 
 read(__dirname + '/input.txt', 'utf8')
     .then(split)
@@ -35,23 +36,60 @@ function locate(routes) {
 }
 
 function addRoute(locations, start, end, distance) {
-    locations[start] = locations[start] || {
-        map: {}
-    };
-    locations[start].map[end] = {
-        distance: distance
+    locations[start] = locations[start] || {};
+    locations[start][end] = {
+        distance: distance,
+        travelled: 0
     };
 }
 
 function solve(locations) {
-    var createMap = function() {
-        return createMapFromLocations(locations);
-    };
-    return locations;
+    var list = Object.keys(locations);
+    var reports = [];
+    list.forEach(function(start) {
+        var location = locations[start];
+        var report = solveLocation(start, location, locations, list, [start], 0);
+        reports = [].concat(reports, report);
+    });
+
+    reports.sort(function(a, b) {
+        return b.travelled - a.travelled;
+    });
+
+    return reports;
+}
+
+function solveLocation(start, location, locations, list, route, travelled) {
+    var reports = [];
+    list.forEach(function(destination) {
+        if (route.indexOf(destination) !== -1) {
+            return;
+        }
+
+        var distance = locations[start][destination].distance;
+        location[destination] = location[destination] || {};
+        location[destination].travelled = travelled + distance;
+
+        if (route.length + 1 === list.length) {
+            var report = {
+                route: [].concat(route, [destination]),
+                travelled: location[destination].travelled
+            };
+            reports.push(report);
+            console.log([report.route.join(' -> '), '=', location[destination].travelled].join(' '));
+        }
+
+        var newReports = solveLocation(destination, location[destination] || {}, locations, list, [].concat(route, [destination]), location[destination].travelled);
+        reports = [].concat(reports, newReports);
+    });
+    return reports;
 }
 
 function report(summary) {
-    console.log('Summary:', JSON.stringify(summary, null, '  '));
+    var file = JSON.stringify(summary, null, '  ');
+    console.log('Summary:', file);
+    write('day9.json', file);
+    return summary;
 }
 
 function error(ex) {
