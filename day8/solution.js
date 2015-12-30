b24dfbacf6ce8af38e953bd2dd27e4cbd178c287
John Beech <github@mkv25.net>
2015-12-30 12:02:45 +0000

    Added encode step to reverse the process

--- day8/solution.js
+++ day8/solution.js
@@ -1,11 +1,25 @@
 var read = require('../lib/read');
 
 console.log('Day 8');
-read(__dirname + '/input.txt', 'utf8')
-    .then(split)
-    .then(solve)
-    .then(report)
+part1()
+    .then(part2)
     .catch(error);
+
+function part1() {
+    console.log('Part 1');
+    return read(__dirname + '/input.txt', 'utf8')
+        .then(split)
+        .then(solve)
+        .then(report);
+}
+
+function part2() {
+    console.log('Part 2');
+    return read(__dirname + '/input.txt', 'utf8')
+        .then(split)
+        .then(encode)
+        .then(report);
+}
 
 function split(input) {
     var lines = input.replace(/\r/g, '').split('\n');
@@ -24,8 +38,8 @@ function solve(lines) {
             parsedLine = parsedLine.replace(/\\\"/g, '-');
             parsedLine = parsedLine.replace(/\\x[A-Fa-f0-9]{2}/g, '#');
             memoryCount = memoryCount + parsedLine.length;
-            console.log([line.length, line].join(' '));
-            console.log([parsedLine.length, '', parsedLine].join(' '));
+            //console.log([line.length, line].join(' '));
+            //console.log([parsedLine.length, '', parsedLine].join(' '));
             return parsedLine;
         } catch (ex) {
             console.log('Unable to parse', line, ex);
@@ -36,6 +50,32 @@ function solve(lines) {
         'String length from input': inputCount,
         'String length in memory': memoryCount,
         'Difference': inputCount - memoryCount
+    }
+}
+
+function encode(lines) {
+    var inputCount = 0;
+    var encodedCount = 0;
+    var parsed = lines.map(function(line) {
+        inputCount = inputCount + line.length;
+        try {
+            var encodedLine = line;
+            encodedLine = encodedLine.replace(/\"/g, '--');
+            encodedLine = encodedLine.replace(/\\/g, '~~');
+            encodedLine = '"' + encodedLine + "'";
+            encodedCount = encodedCount + encodedLine.length;
+            console.log([line.length, line].join(' '));
+            console.log([encodedLine.length, '', encodedLine].join(' '));
+            return encodedLine;
+        } catch (ex) {
+            console.log('Unable to encode', line, ex);
+            return line;
+        }
+    });
+    return {
+        'String length from input': inputCount,
+        'String length when encoded': encodedCount,
+        'Difference': encodedCount - inputCount
     }
 }
 
