c4785e003badfab53d2c8b0bf02d9242d345e6c9
John Beech <github@mkv25.net>
2016-01-01 21:37:38 +0000

    Added parser to extract value from input

--- day13/solution.js
+++ day13/solution.js
@@ -1,12 +1,38 @@
 var read = require('../lib/read');
 
 read(__dirname + '/input.txt', 'utf8')
+    .then(split)
+    .then(parse)
+    .then(index)
     .then(solve)
     .then(report)
     .catch(error);
 
-function solve(lines) {
-    throw 'Not implemented';
+function split(input) {
+    var lines = input.replace(/\r/g, '').split('\n');
+    return Promise.accept(lines);
+}
+
+
+function parse(lines) {
+    var matcher = /([A-z]+) would (gain|lose) ([\d]+) happiness units by sitting next to ([A-z]+)./;
+    return lines.map(function(line) {
+        var matches = line.match(matcher);
+        var multiplier = (matches[2] === 'gain') ? 1.0 : -1.0;
+        return {
+            actor: matches[1],
+            happiness: multiplier * parseInt(matches[3]),
+            neighbour: matches[4]
+        };
+    });
+}
+
+function index(instructions) {
+    return instructions
+}
+
+function solve(index) {
+    return index;
 }
 
 function report(summary) {
