0b733d8a082072bbc44ef870c36c204d92cfe4bd
John Beech <github@mkv25.net>
2016-01-01 21:27:34 +0000

    Added red value detection

--- day12/solution.js
+++ day12/solution.js
@@ -14,17 +14,32 @@ function solve(input) {
 }
 
 function sum(object) {
+    if (!Array.isArray(object) && containsRed(object)) {
+        return 0;
+    }
+
     var total = 0;
     Object.keys(object).forEach(function(key) {
         var value = object[key];
         if (typeof value === 'object') {
             total = total + sum(value);
-        }
-        else if(!isNaN(value)) {
+        } else if (!isNaN(value)) {
             total = total + parseInt(value);
         }
     });
     return total;
+}
+
+function containsRed(object) {
+    var keys = Object.keys(object);
+    while (keys.length > 0) {
+        var key = keys.pop();
+        var value = object[key];
+        if (value === 'red') {
+            return true;
+        }
+    }
+    return false;
 }
 
 function report(summary) {
