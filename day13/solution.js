60fe797b2bf8d6b125be203f90752176bc0cfa80
John Beech <github@mkv25.net>
2016-01-01 22:22:10 +0000

    Created solution for Day 13

--- day13/solution.js
+++ day13/solution.js
@@ -1,5 +1,9 @@
 var read = require('../lib/read');
 
+var happiest = {
+    total: 0
+};
+
 read(__dirname + '/input.txt', 'utf8')
     .then(split)
     .then(parse)
@@ -33,7 +37,8 @@ function index(instructions) {
         index[instruction.actor] = index[instruction.actor] || {
             happiness: 0,
             desired: {},
-            undesired: {}
+            undesired: {},
+            names: {}
         };
 
         if (instruction.happiness > 0) {
@@ -41,13 +46,71 @@ function index(instructions) {
         } else {
             index[instruction.actor].undesired[instruction.neighbour] = instruction.happiness;
         }
+        index[instruction.actor].names[instruction.neighbour] = instruction.happiness;
     });
 
     return index;
 }
 
 function solve(index) {
-    return index;
+    var reports = [];
+    var names = Object.keys(index);
+
+    names.forEach(function(name) {
+        var actor = index[name];
+        var report = solveFor(name, index, names, [name]);
+        reports = reports.concat(report);
+    });
+
+    reports.sort(function(a, b) {
+        return a.total - b.total;
+    });
+
+    return {
+        index,
+        reports
+    };
+}
+
+function solveFor(name, index, names, seatingPlan) {
+    var reports = [];
+    names.forEach(function(neighbour) {
+        if (seatingPlan.indexOf(neighbour) !== -1) {
+            return;
+        }
+
+        var newReports = solveFor(neighbour, index, names, [].concat(seatingPlan, neighbour));
+        reports = [].concat(reports, newReports);
+    });
+
+    if (Object.keys(seatingPlan).length === names.length) {
+        var report = calculateHappiness(seatingPlan, index);
+        if (report.total > happiest.total) {
+            happiest = report;
+            console.log('Happiest ->', seatingPlan.join(' <-> '), '<-', happiest.total);
+            reports.push(report);
+        }
+    }
+
+    return reports;
+}
+
+function calculateHappiness(seatingPlan, index) {
+    var total = 0;
+    var report = {};
+    var current, previous, next;
+    for (var i = 0; i < seatingPlan.length; i++) {
+        current = seatingPlan[i];
+        previous = seatingPlan[(i - 1 + seatingPlan.length) % seatingPlan.length];
+        next = seatingPlan[(i + 1) % seatingPlan.length];
+        report[current] = index[current].names[previous] + index[current].names[next];
+        total = total + report[current];
+    }
+
+    return {
+        report,
+        total
+    };
 }
 
 function report(summary) {
