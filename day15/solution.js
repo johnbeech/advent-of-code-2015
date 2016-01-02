21a24a92f714e7e2a1f84abeb3db113c8ec96a37
John Beech <github@mkv25.net>
2016-01-02 00:40:10 +0000

    Solved for example using numbers from instructions

--- day15/solution.js
+++ day15/solution.js
@@ -1,8 +1,11 @@
 var read = require('../lib/read');
 
+var teaspoons = 100;
+
 read(__dirname + '/input.txt', 'utf8')
     .then(split)
     .then(parse)
+    .then(index)
     .then(solve)
     .then(report)
     .catch(error);
@@ -18,21 +21,88 @@ function parse(lines) {
         var matches = line.match(matcher);
         var ingredient = {
             name: matches[1],
+            properties: {}
         };
         var properties = matches[2].split(', ');
+        console.log(ingredient.name);
         properties.forEach(function(property) {
-            console.log(ingredient.name, property);
+            console.log('', property);
             var s = property.split(' ');
             var key = s[0];
             var value = parseInt(s[1]);
-            ingredient[key] = value;
+            ingredient.properties[key] = value;
         });
+        console.log();
         return ingredient;
     });
 }
 
-function solve(index) {
+function index(ingredients) {
+    var index = {};
+    ingredients.forEach(function(ingredient) {
+        index[ingredient.name] = ingredient.properties;
+    });
     return index;
+}
+
+function solve(index) {
+    var ingredients = Object.keys(index);
+    var recipe = makeRecipe(ingredients, teaspoons);
+
+    return [
+        solveFor(recipe, index),
+        solveFor({
+            Butterscotch: 44,
+            Cinnamon: 56
+        }, index)
+    ];
+}
+
+function makeRecipe(ingredients, teaspoons) {
+    var recipe = {};
+
+    var take = (function() {
+        var remaining = teaspoons;
+        return function(requestedAmount) {
+            var availableAmount = Math.min(requestedAmount, remaining);
+            remaining = remaining - availableAmount;
+            return availableAmount;
+        }
+    })();
+
+    var portion = Math.ceil(teaspoons / ingredients.length);
+    ingredients.forEach(function(ingredient) {
+        recipe[ingredient] = take(portion);
+    });
+
+    return recipe;
+}
+
+function solveFor(recipe, index) {
+    return scoreRecipe(recipe, index);
+}
+
+function scoreRecipe(recipe, index) {
+    var properties = {};
+    var result = {
+        recipe,
+        properties
+    };
+
+    Object.keys(recipe).forEach(function(ingredient) {
+        var quantity = recipe[ingredient];
+        Object.keys(index[ingredient]).forEach(function(property) {
+            properties[property] = (properties[property] || 0) + (index[ingredient][property] * quantity);
+        });
+    });
+
+    Object.keys(properties).forEach(function(property) {
+        properties[property] = Math.max(0, properties[property]);
+    });
+
+    result.score = properties.capacity * properties.durability * properties.flavor * properties.texture;
+
+    return result;
 }
 
 function report(summary) {
